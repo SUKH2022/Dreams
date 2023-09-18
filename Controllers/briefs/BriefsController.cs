ac38c325c3e8a32292a3be50ccf556080c3f6e19
Sukhpreet Saini <sukhpreet.saini2020@gmail.com>
2023-09-18 17:48:08 -0400

    updated_proposal

--- Controllers/briefs/BriefsController.cs
+++ Controllers/briefs/BriefsController.cs
@@ -11,24 +11,32 @@ namespace Dreams
         public IActionResult Index()
         {
             List<Dictionary<string, object>> data = new()
-        {
-            new()
             {
-                {"Id", 1},
-                {"Company Name", "Dreams"},
-                {"Description", "Pioneering tech solutions in healthcare, accessibility, robotics, transportation, security, environment, and space exploration for a better future."},
-                {"Group members & roles", "Sukhpreet Saini- Full Stack Developer"}
-            },
-            new()
+                new()
+                {
+                    {"Id", 1},
+                    {"Company Name", "Dreams"},
+                    {"Description", "Pioneering healthcare AI solutions for a better future."},
+                    {"Group members & roles", "Sukhpreet Saini- Full Stack Developer"}
+                },
+                new()
+                {
+                    {"Id", 2},
+                    {"Company Name", "Dreams"},
+                    {"Description", "Pioneering healthcare AI solutions for a better future."},
+                    {"Group members & roles", "Abin Byju- Front-End Developer"}
+                }
+            };
+
+            ViewData["ProposalData"] = new
             {
-                {"Id", 2},
-                {"Company Name", "Dreams"},
-                {"Description", "Pioneering tech solutions in healthcare, accessibility, robotics, transportation, security, environment, and space exploration for a better future."},
-                {"Group members & roles", "Abin Byju- Front-End Developer"}
-            }
-        };
+                description = "Dreams Innovations is an innovative platform dedicated to transforming lives through cutting-edge healthcare AI technology. We aim to provide a one-stop solution for individuals seeking groundbreaking advancements in healthcare. At Dreams Innovations, we believe in pushing the boundaries of what's possible to create a better future for all.",
+                ProductsOrServices = "Dreams Innovations provides AI-powered healthcare solutions.",
+                IndustryNeed = "Dreams Innovations fulfills a crucial need in the healthcare industry by harnessing AI technology to improve early disease detection, treatment recommendations, and healthcare insights. Our solutions address pressing challenges and drive innovation in the healthcare sector, ultimately benefiting individuals and communities.",
+                UserRoles = "User roles at Dreams Innovations include admins responsible for platform management, customers with access to healthcare AI solutions, and members who engage in community forums and receive updates on healthcare initiatives."
+            };
+
             return View(data);
         }
     }
-
 }
