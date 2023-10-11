81cc6613b43f5370bf0bc75bb9e69858f8988611
Sukhpreet Saini <sukhpreet.saini2020@gmail.com>
2023-10-11 18:23:32 -0400

    contact form is the bonus

---
+++ code generate  model and view.ps1
@@ -0,0 +1,3 @@
+dotnet aspnet-codegenerator controller -name CategoriesController -m Category -dc ApplicationDbContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries
+
+dotnet aspnet-codegenerator controller -name CategoryProductsController -m CategoryProduct -dc ApplicationDbContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries
--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -5,3 +5,25 @@ let spanTexts=document.getElementsByTagName("span");
                 spanText.classList.add("active");
             }
         }
+
+        // document.getElementById('searchInput').addEventListener('input', function () {
+        //     console.log("Search input changed"); // Debugging statement
+        
+        //     var input, filter, tr, td, i, txtValue;
+        //     input = document.getElementById('searchInput');
+        //     filter = input.value.toUpperCase();
+        //     tr = document.querySelectorAll('table tbody tr');
+        
+        //     for (i = 0; i < tr.length; i++) {
+        //         td = tr[i].getElementsByTagName('td')[0];
+        //         if (td) {
+        //             txtValue = td.textContent || td.innerText;
+        //             if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        //                 tr[i].style.display = ''; // Show rows that match the search
+        //             } else {
+        //                 tr[i].style.display = 'none'; // Hide rows that don't match
+        //             }
+        //         }
+        //     }
+        // });
+        
